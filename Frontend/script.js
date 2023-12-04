c999a83147b545e5480f46c53cd551cbe8f06fb9
MaryanQ <maryansalad7@outlook.dk>
2023-12-04 08:45:38 +0100

    xx

--- Frontend/script.js
+++ Frontend/script.js
@@ -12,21 +12,21 @@ async function start() {
 
 async function Getartists() {
   const response = await fetch(`
-http://localhost:3000/artists`);
+https://musicbaseee.azurewebsites.net/artists`);
   artists = await response.json();
   displayartists(artists);
 }
 
 async function GetAlbums() {
   const response = await fetch(`
-http://localhost:3000/albums`);
+https://musicbaseee.azurewebsites.net/albums`);
   allAlbums = await response.json();
   displayAlbums(allAlbums);
 }
 
 async function GetTracks() {
   const response = await fetch(`
-http://localhost:3000/tracks`);
+https://musicbaseee.azurewebsites.net/tracks`);
   allTracks = await response.json();
   displayTracks(allTracks);
 }
